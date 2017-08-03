6733fcef156e281701e2c5a538482866dde37ecf
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-02 21:24:23 -0400

    Make the Metadata class singleton

---
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/base/KitchenSinkApp.java
@@ -0,0 +1,8 @@
+package dev.learn.phoenix.app.androidkitchensink.base;
+
+/**
+ * Created by sudharti on 8/2/17.
+ */
+
+public class KitchenSinkApp {
+}
--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/common/Metadata.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/common/Metadata.java
@@ -1,24 +1,81 @@
 package dev.learn.phoenix.app.androidkitchensink.common;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import dev.learn.phoenix.app.androidkitchensink.shared.ContainerListItem;
+import dev.learn.phoenix.app.androidkitchensink.topic_layouts_views.FrameLayoutFragment;
+import dev.learn.phoenix.app.androidkitchensink.topic_layouts_views.LinearLayoutFragment;
+import dev.learn.phoenix.app.androidkitchensink.topic_layouts_views.PercentRelativeLayoutFragment;
+import dev.learn.phoenix.app.androidkitchensink.topic_layouts_views.RelativeLayoutFragment;
 
 /**
  * Created by sudhar on 8/2/17.
  */
 
 public class Metadata {
-	private static Map<String, Map<String, Class>> metadataMap;
 
-	public static void init() {
-		metadataMap = new LinkedHashMap<String, Map<String, Class>>();
-	}
+    private static final Metadata sMetadata = new Metadata();
+    private Map<String, Map<String, List<ContainerListItem>>> metadataMap;
 
-	public static Map<String, Class> getSection(String key) {
+    public static Metadata getInstance() {
+        return sMetadata;
+    }
 
-	}
+    public void init() {
+        metadataMap = new LinkedHashMap<>();
+        loadMetadata();
+    }
 
-	private void loadMap() {
-		
-	}
+    public Map<String, Map<String, List<ContainerListItem>>> getMetadata() {
+        return metadataMap;
+    }
+
+    public Map<String, List<ContainerListItem>> getTopic(String topic) {
+        return metadataMap.get(topic);
+    }
+
+    private void loadMetadata() {
+        String[] topics = {"Views & Layouts", "Styling Views", "Adapter Views", "Event Handling",
+                "Networking", "Persistence", "Background Jobs/Services", "Intents",
+                "Image Handling", "Surface Views", "Sensors"};
+        for (String topic : topics) {
+            metadataMap.put(topic, new LinkedHashMap<String, List<ContainerListItem>>());
+        }
+
+        populateViewsAndLayouts(metadataMap);
+    }
+
+    private void populateViewsAndLayouts(Map<String, Map<String, List<ContainerListItem>>> metadataMap) {
+        Map<String, List<ContainerListItem>> sectionsMap = metadataMap.get("Views & Layouts");
+        sectionsMap.put("Layouts", getLayoutsList());
+        sectionsMap.put("Views", getViewsList());
+    }
+
+    private List<ContainerListItem> getLayoutsList() {
+        ContainerListItem[] containerListItems = new ContainerListItem[]{
+                new ContainerListItem("Linear Layout", LinearLayoutFragment.class),
+                new ContainerListItem("Relative Layout", RelativeLayoutFragment.class),
+                new ContainerListItem("Percentage Relative Layout", PercentRelativeLayoutFragment.class),
+                new ContainerListItem("Frame Layout", FrameLayoutFragment.class)
+        };
+
+        return constructList(containerListItems);
+    }
+
+    private List<ContainerListItem> getViewsList() {
+        ContainerListItem[] containerListItems = new ContainerListItem[]{};
+        return constructList(containerListItems);
+    }
+
+
+    private List<ContainerListItem> constructList(ContainerListItem[] containerListItems) {
+        List<ContainerListItem> containerListItemsList = new ArrayList<>();
+        for (ContainerListItem containerListItem : containerListItems) {
+            containerListItemsList.add(containerListItem);
+        }
+        return containerListItemsList;
+    }
 }
