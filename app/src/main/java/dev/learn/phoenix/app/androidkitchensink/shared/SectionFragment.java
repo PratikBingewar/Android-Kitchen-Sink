c6964762d3d46cf4d0e515d2b587a6731f26c966
Sudharsanan <sudhar.ti@gmail.com>
2017-08-04 17:42:16 -0400

    Fix a bunch of stuff with fragments when activity being restarted

--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/SectionFragment.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/SectionFragment.java
@@ -3,10 +3,15 @@ package dev.learn.phoenix.app.androidkitchensink.shared;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import java.io.Serializable;
+import java.util.List;
 
 import dev.learn.phoenix.app.androidkitchensink.R;
 import dev.learn.phoenix.app.androidkitchensink.common.Section;
@@ -15,20 +20,55 @@ import dev.learn.phoenix.app.androidkitchensink.common.Section;
  * Created by sudharti on 8/3/17.
  */
 
-public class SectionFragment extends Fragment {
-    private Section mSection;
-    private ListView mListView;
+public class SectionFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-    public static SectionFragment newInstance(Section section) {
-        SectionFragment sectionFragment = new SectionFragment();
-        sectionFragment.mSection = section;
-        return sectionFragment;
-    }
+	private static final String SECTION = "SECTION";
+	private static final String TAG = SectionFragment.class.getSimpleName();
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mListView = (ListView) inflater.inflate(R.layout.fragment_section, container, false);
-        return mListView;
-    }
+	private ListView mListView;
+
+	private Section mSection;
+	private List<SectionListItem> mSectionListItems;
+	private SectionListAdapter mSectionListAdapter;
+
+	public static SectionFragment newInstance(Section section) {
+		SectionFragment sectionFragment = new SectionFragment();
+
+		Bundle args = new Bundle();
+		args.putSerializable(SECTION, (Serializable) section);
+		sectionFragment.setArguments(args);
+
+		return sectionFragment;
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.mSection = (Section) getArguments().getSerializable(SECTION);
+		this.mSectionListItems = mSection.getSectionListItems();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		mListView = (ListView) inflater.inflate(R.layout.fragment_section, container, false);
+		mSectionListAdapter = new SectionListAdapter(getContext(), this.mSectionListItems);
+		mListView.setAdapter(mSectionListAdapter);
+		mListView.setOnItemClickListener(this);
+		return mListView;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+		SectionListItem sectionListItem = mSectionListItems.get(i);
+		Class clazz = sectionListItem.getClazz();
+		try {
+			getActivity().getSupportFragmentManager().beginTransaction()
+					.replace(R.id.frame_main_content, (Fragment) clazz.newInstance())
+					.addToBackStack(sectionListItem.getText()).commit();
+		} catch (java.lang.InstantiationException | IllegalAccessException e) {
+			Log.e(TAG, e.getMessage());
+		}
+
+	}
 }
--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/TopicFragment.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/TopicFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 
 import dev.learn.phoenix.app.androidkitchensink.R;
@@ -18,23 +20,35 @@ import dev.learn.phoenix.app.androidkitchensink.common.Section;
  */
 public class TopicFragment extends Fragment {
 
-    private ViewPager mViewPager;
-    private Map<Integer, Section> mSectionsMap;
-    private TopicViewPagerAdapter mViewPagerAdapter;
+	private static final String SECTIONS = "SECTIONS";
 
-    public static TopicFragment newInstance(Map<Integer, Section> sectionsMap) {
-        TopicFragment topicFragment = new TopicFragment();
-        topicFragment.mSectionsMap = sectionsMap;
-        return topicFragment;
-    }
+	private ViewPager mViewPager;
+	private List<Section> mSectionsList;
+	private TopicViewPagerAdapter mViewPagerAdapter;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_container, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.view_pager);
-        mViewPagerAdapter = new TopicViewPagerAdapter(getFragmentManager(), mSectionsMap);
-        mViewPager.setAdapter(mViewPagerAdapter);
-        return view;
-    }
+	public static TopicFragment newInstance(List<Section> sectionsList) {
+		TopicFragment topicFragment = new TopicFragment();
+
+		Bundle args = new Bundle();
+		args.putSerializable(SECTIONS, (Serializable) sectionsList);
+		topicFragment.setArguments(args);
+
+		return topicFragment;
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.mSectionsList = (List<Section>) getArguments().getSerializable(SECTIONS);
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_container, container, false);
+		mViewPager = (ViewPager) view.findViewById(R.id.view_pager);
+		mViewPagerAdapter = new TopicViewPagerAdapter(getFragmentManager(), mSectionsList);
+		mViewPager.setAdapter(mViewPagerAdapter);
+		return view;
+	}
 }
