746c6ca972320221248a9c7cd242f3f9e61d0985
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-07 01:16:59 -0400

    Add ProgressBar fragment

--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/ProgressBarFragment.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/shared/ProgressBarFragment.java
@@ -1,10 +1,29 @@
 package dev.learn.phoenix.app.androidkitchensink.shared;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dev.learn.phoenix.app.androidkitchensink.R;
 
 /**
  * Created by sudharti on 8/7/17.
  */
 
 public class ProgressBarFragment extends Fragment {
+
+    private static final ProgressBarFragment mProgressBarFragment = new ProgressBarFragment();
+
+    public static ProgressBarFragment getInstance() {
+        return mProgressBarFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_progress_bar, container, false);
+    }
 }
