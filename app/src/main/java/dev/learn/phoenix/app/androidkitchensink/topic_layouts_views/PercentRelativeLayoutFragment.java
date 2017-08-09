070f3ab66cafbd2ea4e85dfca74c579bcf29c3cb
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-08 22:58:21 -0400

    Finish up Percent relative layout

--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/topic_layouts_views/PercentRelativeLayoutFragment.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/topic_layouts_views/PercentRelativeLayoutFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import dev.learn.phoenix.app.androidkitchensink.R;
+
 /**
  * Created by sudharti on 7/27/17.
  */
@@ -15,6 +17,6 @@ public class PercentRelativeLayoutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_percent_relative_layout, container, false);
     }
 }
