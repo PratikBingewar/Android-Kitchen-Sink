0e4655473576f9161828106d8473c01f0e1e86ea
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-11 22:03:17 -0400

    Add TextViewFragment

--- app/src/main/java/dev/learn/phoenix/app/androidkitchensink/topic_layouts_views/TextViewFragment.java
+++ app/src/main/java/dev/learn/phoenix/app/androidkitchensink/topic_layouts_views/TextViewFragment.java
@@ -1,10 +1,34 @@
 package dev.learn.phoenix.app.androidkitchensink.topic_layouts_views;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dev.learn.phoenix.app.androidkitchensink.R;
 
 /**
  * Created by sudharti on 8/5/17.
  */
 
 public class TextViewFragment extends Fragment {
+
+    private ActionBar mActionBar;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_text_view, container, false);
+        return view;
+    }
 }
